43b0f5d09cd0d77a88b2447650ace910e7f74965
TheSpatializer <webmaster.thespatializer@gmail.com>
2017-09-17 23:31:59 +0200

    Added cleanUpFolders script
    This python script offers to delete every file (unless its name starts with ".") in a given directory. It ignores subdirectories for now.
    Be careful using it with Windows as path is not very practical to give ("\" being an escape character).
    This is version 1.0 of this scripts, features are to be added in the future.

---
+++ cleanUpFolders.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python3
+"""
+cleanUpFolder.py v1.0 - 2017 Copyright: TheSpatializer - Apache License 2.0
+This script has been written in Python 3.5 and can be used for educational purpose only.
+Edition is encouraged as well as remarks and ideas for futur improvement.
+Source : 
+Please, link the source of the code you use and do not try to make money out of others' work.
+------------------------------Credits----------------------------
+Cheers to Martin Thoma (https://stackoverflow.com/users/562769/martin-thoma)
+for his answer on this thread: https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+"""
+from os import listdir, remove
+from os.path import isfile, join
+
+def cleanFilesList(filesList):
+	filesList.remove("cleanUp.py")
+	for i in filesList:
+		if i[0]=='.':
+			files.remove(i)
+	return(files)
+
+def areThereFolders(onlyFiles,foldContent):
+	if len(onlyFiles)<len(foldContent):
+		ret=len(foldContent)-len(onlyFiles)
+	elif len(onlyFiles)==len(foldContent):
+		ret=0
+	else:
+		ret="Error 42"
+	return(ret)
+
+# Change value for the directory you want to clean
+folder="Your/path/here"
+
+# Create list of all unhidden files in the folder
+files = [f for f in listdir(folder) if isfile(join(folder, f))]
+cleanFilesList(files)
+# Create list of all the first level unhidden content in the folder
+filesxfolders = listdir(folder)
+cleanFilesList(filesxfolders)
+# Comparing lists to detect folders
+nbFolders=areThereFolders(files,filesxfolders)
+
+# Avoiding problems
+if type(nbFolders) is str:
+	input("There seems to be a problem here, don't want to know about it!\nPress Enter to leave!!")
+
+else:
+	# Some little chit-chat!
+	print("Do you want the following files to be deleted?:")
+	for i in files:
+		print(i)
+	ans=''
+	while ans!='y' and ans!='n':
+		ans = input("(y/n): ")
+	
+	if ans=='y':
+		# Deleting files (only files)
+		for i in files:
+			remove(folder+"/"+i)
+		print("\nThe files have been removed from your computer.")
+		
+		# [Work in progress] About subdirectories
+		if nbFolders>0:
+			if nbFolders<2:
+				print("\n"+str(nbFolders)+"folder has been detected in this directory")
+			elif nbFolders>1:
+				print("\n"+str(nbFolders)+"folders have been detected in this directory")
+			print("This version of the program doesn't allow to purge them,")
+			print("you will have to do it yourself.\n")
+		#Bye-bye
+		input("Press Enter to leave...")
+	
+	elif ans=='n':
+		# Why even run the script?!
+		input("\nPress Enter to leave.")
