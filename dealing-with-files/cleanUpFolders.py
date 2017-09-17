434ce9a7dca52d37b95a62ab78b1077d63526faf
TheSpatializer <webmaster.thespatializer@gmail.com>
2017-09-18 00:09:59 +0200

    Updated cleanUpFolders v.1.1
    Fixed some typos, corrected some old and unchanged variables name and added a pre-written path for Windows users (because I am your humble servant ;) )

--- dealing-with-files/cleanUpFolders.py
+++ dealing-with-files/cleanUpFolders.py
@@ -2,7 +2,7 @@
 """
 cleanUpFolder.py v1.0 - 2017 Copyright: TheSpatializer - Apache License 2.0
 This script has been written in Python 3.5 and can be used for educational purpose only.
-Edition is encouraged as well as remarks and ideas for futur improvement.
+Edition is encouraged as well as remarks and ideas for future improvement.
 Source : 
 Please, link the source of the code you use and do not try to make money out of others' work.
 ------------------------------Credits----------------------------
@@ -13,10 +13,10 @@ from os import listdir, remove
 from os.path import isfile, join
 
 def cleanFilesList(filesList):
-	filesList.remove("cleanUp.py")
+	filesList.remove("cleanUpFolders.py")
 	for i in filesList:
 		if i[0]=='.':
-			files.remove(i)
+			filesList.remove(i)
 	return(files)
 
 def areThereFolders(onlyFiles,foldContent):
@@ -29,7 +29,7 @@ def areThereFolders(onlyFiles,foldContent):
 	return(ret)
 
 # Change value for the directory you want to clean
-folder="Your/path/here"
+folder="/Users/webmaster/Downloads/test"
 
 # Create list of all unhidden files in the folder
 files = [f for f in listdir(folder) if isfile(join(folder, f))]
@@ -45,31 +45,47 @@ if type(nbFolders) is str:
 	input("There seems to be a problem here, don't want to know about it!\nPress Enter to leave!!")
 
 else:
-	# Some little chit-chat!
-	print("Do you want the following files to be deleted?:")
-	for i in files:
-		print(i)
-	ans=''
-	while ans!='y' and ans!='n':
-		ans = input("(y/n): ")
-	
-	if ans=='y':
-		# Deleting files (only files)
-		for i in files:
-			remove(folder+"/"+i)
-		print("\nThe files have been removed from your computer.")
-		
-		# [Work in progress] About subdirectories
+	# The folder is empty of files
+	if len(files)==0:
+		print("There does not seem to be any file in this directory.")
+		# See below --> l.78
 		if nbFolders>0:
 			if nbFolders<2:
-				print("\n"+str(nbFolders)+"folder has been detected in this directory")
+				print("\n"+str(nbFolders)+" folder has been detected in this directory.")
 			elif nbFolders>1:
-				print("\n"+str(nbFolders)+"folders have been detected in this directory")
+				print("\n"+str(nbFolders)+" folders have been detected in this directory.")
 			print("This version of the program doesn't allow to purge them,")
 			print("you will have to do it yourself.\n")
 		#Bye-bye
 		input("Press Enter to leave...")
 	
-	elif ans=='n':
-		# Why even run the script?!
-		input("\nPress Enter to leave.")
+	# The user is not dumb
+	else:
+		# Some little chit-chat!
+		print("Do you want the following files to be deleted?:")
+		for i in files:
+			print(i)
+		ans=''
+		while ans!='y' and ans!='n':
+			ans = input("(y/n): ")
+		
+		if ans=='y':
+			# Deleting files (only files)
+			for i in files:
+				remove(folder+"/"+i)
+			print("\nThe files have been removed from your computer.")
+			
+			# [Work in progress] About subdirectories
+			if nbFolders>0:
+				if nbFolders<2:
+					print("\n"+str(nbFolders)+" folder has been detected in this directory.")
+				elif nbFolders>1:
+					print("\n"+str(nbFolders)+" folders have been detected in this directory.")
+				print("This version of the program doesn't allow to purge them,")
+				print("you will have to do it yourself.\n")
+			#Bye-bye
+			input("Press Enter to leave...")
+		
+		elif ans=='n':
+			# Why even run the script?!
+			input("\nPress Enter to leave.")
